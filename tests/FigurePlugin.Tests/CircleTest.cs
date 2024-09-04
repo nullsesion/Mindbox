b68b62b28c33bd40519da8b8eaa02f4a81a0e354
nullsesion <nullsesion@yandex.ru>
2024-09-04 20:08:32 +0300

    add tests

---
+++ tests/FigurePlugin.Tests/CircleTest.cs
@@ -0,0 +1,31 @@
+﻿using Contracts.Primitives;
+using FigurePlugin.Figures;
+using Moq;
+
+namespace FigurePlugin.Tests
+{
+	[TestClass]
+	public class CircleTest
+	{
+		[TestMethod]
+		public void Try_GetArea__Pnt_0_0_R1__double_PI_Returned()
+		{
+			Mock<IPoint> center = new Mock<IPoint>();
+			center.Setup(x => x.X).Returns(0d);
+			center.Setup(x => x.Y).Returns(0d);
+			
+			Circle circle = new Circle();
+			circle.Points = new IPoint[] { center.Object };
+			circle.Radius = 1d;
+
+			if (circle.TryGetArea(out double area))
+			{
+				Assert.AreEqual(Math.PI, area);
+			}
+			else
+			{
+				Assert.Fail("an error occurred calc area");
+			}
+		}
+	}
+}
---
+++ tests/FigurePlugin.Tests/TriangleTest.cs
@@ -0,0 +1,49 @@
+﻿using Contracts.Primitives;
+using FigurePlugin.Figures;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigurePlugin.Tests
+{
+	[TestClass]
+	public class TriangleTest
+	{
+		[TestMethod]
+		public void Try_GetArea__Pnt_0_0_Pnt_0_3_Pnt_5_0__double_6_Returned()
+		{
+			Mock<IPoint> point1 = new Mock<IPoint>();
+			point1.Setup(x => x.X).Returns(0d);
+			point1.Setup(x => x.Y).Returns(0d);
+
+			Mock<IPoint> point2 = new Mock<IPoint>();
+			point2.Setup(x => x.X).Returns(0d);
+			point2.Setup(x => x.Y).Returns(3d);
+
+			Mock<IPoint> point3 = new Mock<IPoint>();
+			point3.Setup(x => x.X).Returns(4d);
+			point3.Setup(x => x.Y).Returns(3d);
+
+			IPoint[] points = new IPoint[]
+			{
+				point1.Object,
+				point2.Object,
+				point3.Object,
+			};
+
+			Triangle triangle = new Triangle();
+			triangle.Points = points;
+			if (triangle.TryGetArea(out double area))
+			{
+				Assert.AreEqual(6, area);
+			}
+			else
+			{
+				Assert.Fail("an error occurred calc area");
+			}
+		}
+	}
+}
